05d1e4fdf5d0c69c38af40d0085bdc6070bd3622
Anand-Jeeva <62829696+Anand-Jeeva@users.noreply.github.com>
2021-01-24 11:42:34 +0530

    jokesfortoday

---
+++ script.js
@@ -0,0 +1,22 @@
+
+	function newOne(){
+		let ra=Math.floor(Math.random()*12);
+		//console.log(random);
+		console.log(ra);
+		document.getElementById('sololearn').innerHTML=jo[ra];
+		
+	};
+	var jo=[
+	'Two bytes meet.<br><br>The first byte asks,Are your ill?<br><br>The second byte replies,No,just feeling a bit off.',
+	'Pythn:I am Python Develper<br><br>	C: Python made by c',
+	'who was the first to spam on sololearn?<br><br>Alext Babu',
+	'why did the programmer quit his job? <br><br> Because he didnt get array',
+	'A SQL query goes into a bar walks up to two tables and asks.. <br><br>can i join uour?',
+	'id like to make the world a better place.. </br></br>but they wont give me the source code.',
+	'/"just saw my life flash before my eyes and all i could see was a close tag"',
+	'debugging: removeing the needles from the haystack.',
+	'From the random shack data processing Dictionary </br></br> Endless loop: n.. see loop,Endless.<br>Loop endless: n.,see endless Loop.'
+	'One Programmer ask me how to ran java Program',
+	'husbend: Think in night</br> wift: he is thinking another girl</br> but husben thing if my son give a name to java i am father of java',
+	'Full stack developer asked to me how was web work'
+	]
